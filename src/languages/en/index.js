ea5b93f0daa1ec7143992fc1a3d590760e1b5522
mylenasayumi <mylena.sayumi@gmail.com>
2026-05-04 15:20:04 +0200

    Adjusting the code to the backend likes_count

--- src/languages/en/index.js
+++ src/languages/en/index.js
@@ -3,6 +3,7 @@ import articles from "./articles";
 import auth from "./auth";
 import errorPage from "./errorPage";
 import footer from "./footer";
+import likes from "./likes";
 import login from "./login";
 import navbar from "./navbar";
 import newsletter from "./newsletter";
@@ -22,6 +23,7 @@ const en = {
   aboutUs,
   auth,
   newsletter,
+  likes,
 };
 
 export default en;
